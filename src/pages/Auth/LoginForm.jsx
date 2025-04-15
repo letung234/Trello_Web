6480c5f67ae65b2c4e8ca724d0e396563bf9da5e
Lê Thanh Tùng <161486435+letung234@users.noreply.github.com>
2025-04-15 22:56:34 +0700

    Update LoginForm.jsx

--- src/pages/Auth/LoginForm.jsx
+++ src/pages/Auth/LoginForm.jsx
@@ -46,91 +46,91 @@ function LoginForm() {
     })
   }
 
-  return (
-    <form onSubmit={handleSubmit(submitLogIn)}>
-      <Zoom in={true} style={{ transitionDelay: '200ms' }}>
-        <MuiCard sx={{ minWidth: 380, maxWidth: 380, marginTop: '6em' }}>
-          <Box sx={{
-            margin: '1em',
-            display: 'flex',
-            justifyContent: 'center',
-            gap: 1
-          }}>
-            <Avatar sx={{ bgcolor: 'primary.main' }}><LockIcon /></Avatar>
-            <Avatar sx={{ bgcolor: 'primary.main' }}><TrelloIcon /></Avatar>
-          </Box>
-          <Box sx={{ marginTop: '1em', display: 'flex', justifyContent: 'center', color: theme => theme.palette.grey[500] }}>
-            Author: TrungQuanDev
-          </Box>
-          <Box sx={{ marginTop: '1em', display: 'flex', justifyContent: 'center', flexDirection: 'column', padding: '0 1em' }}>
-            {verifiedEmail &&
-              <Alert severity="success" sx={{ '.MuiAlert-message': { overflow: 'hidden' } }}>
-                Your email&nbsp;
-                <Typography variant="span" sx={{ fontWeight: 'bold', '&:hover': { color: '#fdba26' } }}>{verifiedEmail}</Typography>
-                &nbsp;has been verified.<br />Now you can login to enjoy our services! Have a good day!
-              </Alert>
-            }
-            {registeredEmail &&
-              <Alert severity="info" sx={{ '.MuiAlert-message': { overflow: 'hidden' } }}>
-                An email has been sent to&nbsp;
-                <Typography variant="span" sx={{ fontWeight: 'bold', '&:hover': { color: '#fdba26' } }}>{registeredEmail}</Typography>
-                <br />Please check and verify your account before logging in!
-              </Alert>
-            }
-          </Box>
-          <Box sx={{ padding: '0 1em 1em 1em' }}>
-            <Box sx={{ marginTop: '1em' }}>
-              <TextField
-                autoFocus
-                fullWidth
-                label="Enter Email..."
-                type="text"
-                variant="outlined"
-                error={!!errors['email']}
-                {...register('email', {
-                  required: FIELD_REQUIRED_MESSAGE,
-                  pattern: { value: EMAIL_RULE, message: EMAIL_RULE_MESSAGE }
-                })}
-              />
-              <FieldErrorAlert errors={errors} fieldName={'email'}/>
-            </Box>
-            <Box sx={{ marginTop: '1em' } }>
-              <TextField
-                fullWidth
-                label="Enter Password..."
-                type="password"
-                variant="outlined"
-                error={!!errors['password']}
-                {...register('password', {
-                  required: FIELD_REQUIRED_MESSAGE,
-                  pattern: { value: PASSWORD_RULE, message: PASSWORD_RULE_MESSAGE }
-                })}
-              />
-              <FieldErrorAlert errors={errors} fieldName={'password'}/>
-            </Box>
-          </Box>
-          <CardActions sx={{ padding: '0 1em 1em 1em' }}>
-            <Button
-              className='interceptor-loading'
-              type="submit"
-              variant="contained"
-              color="primary"
-              size="large"
+ return (
+  <form onSubmit={handleSubmit(submitLogIn)}>
+    <Zoom in={true} style={{ transitionDelay: '200ms' }}>
+      <MuiCard sx={{ minWidth: 380, maxWidth: 380, marginTop: '6em' }}>
+        <Box sx={{
+          margin: '1em',
+          display: 'flex',
+          justifyContent: 'center',
+          gap: 1
+        }}>
+          <Avatar sx={{ bgcolor: 'primary.main' }}><LockIcon /></Avatar>
+          <Avatar sx={{ bgcolor: 'primary.main' }}><TrelloIcon /></Avatar>
+        </Box>
+        <Box sx={{ marginTop: '1em', display: 'flex', justifyContent: 'center', flexDirection: 'column', padding: '0 1em' }}>
+          {verifiedEmail &&
+            <Alert severity="success" sx={{ '.MuiAlert-message': { overflow: 'hidden' } }}>
+              Email của bạn&nbsp;
+              <Typography variant="span" sx={{ fontWeight: 'bold', '&:hover': { color: '#fdba26' } }}>{verifiedEmail}</Typography>
+              &nbsp;đã được xác minh.<br />Giờ bạn có thể đăng nhập và sử dụng dịch vụ của chúng tôi. Chúc bạn một ngày tốt lành!
+            </Alert>
+          }
+          {registeredEmail &&
+            <Alert severity="info" sx={{ '.MuiAlert-message': { overflow: 'hidden' } }}>
+              Một email đã được gửi đến&nbsp;
+              <Typography variant="span" sx={{ fontWeight: 'bold', '&:hover': { color: '#fdba26' } }}>{registeredEmail}</Typography>
+              <br />Vui lòng kiểm tra và xác minh tài khoản trước khi đăng nhập!
+            </Alert>
+          }
+        </Box>
+        <Box sx={{ padding: '0 1em 1em 1em' }}>
+          <Box sx={{ marginTop: '1em' }}>
+            <TextField
+              autoFocus
               fullWidth
-            >
-              Login
-            </Button>
-          </CardActions>
-          <Box sx={{ padding: '0 1em 1em 1em', textAlign: 'center' }}>
-            <Typography>New to Trello MERN Stack Advanced?</Typography>
-            <Link to="/register" style={{ textDecoration: 'none' }}>
-              <Typography sx={{ color: 'primary.main', '&:hover': { color: '#ffbb39' } }}>Create account!</Typography>
-            </Link>
+              label="Nhập email..."
+              type="text"
+              variant="outlined"
+              error={!!errors['email']}
+              {...register('email', {
+                required: 'Vui lòng nhập email!',
+                pattern: { value: EMAIL_RULE, message: 'Email không hợp lệ!' }
+              })}
+            />
+            <FieldErrorAlert errors={errors} fieldName={'email'} />
           </Box>
-        </MuiCard>
-      </Zoom>
-    </form>
-  )
+          <Box sx={{ marginTop: '1em' }}>
+            <TextField
+              fullWidth
+              label="Nhập mật khẩu..."
+              type="password"
+              variant="outlined"
+              error={!!errors['password']}
+              {...register('password', {
+                required: 'Vui lòng nhập mật khẩu!',
+                pattern: { value: PASSWORD_RULE, message: 'Mật khẩu không hợp lệ!' }
+              })}
+            />
+            <FieldErrorAlert errors={errors} fieldName={'password'} />
+          </Box>
+        </Box>
+        <CardActions sx={{ padding: '0 1em 1em 1em' }}>
+          <Button
+            className='interceptor-loading'
+            type="submit"
+            variant="contained"
+            color="primary"
+            size="large"
+            fullWidth
+          >
+            Đăng nhập
+          </Button>
+        </CardActions>
+        <Box sx={{ padding: '0 1em 1em 1em', textAlign: 'center' }}>
+          <Typography>Bạn mới sử dụng Trello MERN Stack Advanced?</Typography>
+          <Link to="/register" style={{ textDecoration: 'none' }}>
+            <Typography sx={{ color: 'primary.main', '&:hover': { color: '#ffbb39' } }}>
+              Tạo tài khoản ngay!
+            </Typography>
+          </Link>
+        </Box>
+      </MuiCard>
+    </Zoom>
+  </form>
+)
+
 }
 
 export default LoginForm
